84bda92c8750b67267cc0e3997fbb24f4058124c
kooi nam ng <kooinamng@koois-MacBook-Air.local>
2018-12-26 01:07:07 +0800

    redis lock

---
+++ app/models/redis_lock.rb
@@ -0,0 +1,37 @@
+class RedisLock
+  extend ActiveSupport::Concern
+
+  def self.get_instance
+    redis = Redis.new(url: Rails.application.secrets.redis_url, password: Rails.application.secrets.redis_password)
+
+    self.nsp = Redis::Namespace.new(Rails.application.secrets.redis_namespace, redis: redis)
+  end
+
+  def self.lock(key, expire: 10)
+    redis = self.get_instance
+
+    wait_duration = 0.01
+    expire_in = DateTime.now + expire.seconds
+
+    while redis.setnx(key, expire_in) == false
+      time = redis.get(key)
+
+      if time
+        time = DateTime.parse(time)
+
+        if DateTime.now >= time
+          Rails.logger.error "ALERT REDIS LOCK #{key}"
+          redis.del(key)
+        end
+      end
+
+      sleep wait_duration
+    end
+  end
+
+  def self.unlock(key)
+    redis = self.get_instance
+
+    redis.del(key)
+  end
+end
