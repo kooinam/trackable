203d36ccb9c9f26c9b307e6c5e28772ed6b657fd
kooi nam ng <kooinamng@koois-MacBook-Air.local>
2019-01-03 14:46:14 +0800

    eager

--- app/models/eager_loader.rb
+++ app/models/eager_loader.rb
@@ -7,22 +7,42 @@ class EagerLoader
     eager_loader.total_count = collection.try(:total_count)
 
     args.each do |arg|
-      arg_id = "#{arg}_id".to_sym
-      arg_ids = collection.map do |record|
-        record.send(arg_id)
-      end
+      if arg.is_a? Hash
+        arg.keys.each do |key|
+          eager_loader.all = self.set_relation(eager_loader.all, key)
 
-      relations = arg.to_s.singularize.camelize.constantize.where(:id.in => arg_ids)
-      relations = relations.index_by(&:id)
+          arg[key].each do |nested_key|
+            nested_all = eager_loader.all.map do |record|
+              record.send(key)
+            end
 
-      eager_loader.all = eager_loader.map do |record|
-        record.set_relation(arg, relations[record.send(arg_id)])
-
-        record
+            nested_all = self.set_relation(nested_all, nested_key)
+          end
+        end
+      else
+        eager_loader.all = self.set_relation(eager_loader.all, arg)
       end
     end
 
     eager_loader
+  end
+
+  def self.set_relation(records, key)
+    key_id = "#{key}_id".to_sym
+    key_ids = records.map do |record|
+      record.send(key_id)
+    end
+
+    relations = key.to_s.singularize.camelize.constantize.where(:id.in => key_ids)
+    relations = relations.index_by(&:id)
+
+    records = records.map do |record|
+      record.set_relation(key, relations[record.send(key_id)])
+
+      record
+    end
+
+    records
   end
 
   def each(&proc)
