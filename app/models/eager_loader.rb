e02589b60bdccfb9ddd96fbc9899cf24ef6634b8
kooi nam ng <kooinamng@koois-MacBook-Air.local>
2019-01-03 14:00:26 +0800

    eager loader

--- app/models/eager_loader.rb
+++ app/models/eager_loader.rb
@@ -1,5 +1,11 @@
 class EagerLoader
+  attr_accessor :all, :total_count
+
   def self.load(collection, *args)
+    eager_loader = EagerLoader.new
+    eager_loader.all = []
+    eager_loader.total_count = collection.total_count
+
     args.each do |arg|
       arg_id = "#{arg}_id".to_sym
       arg_ids = collection.map do |record|
@@ -9,13 +15,13 @@ class EagerLoader
       relations = arg.to_s.singularize.camelize.constantize.where(:id.in => arg_ids)
       relations = relations.index_by(&:id)
 
-      collection.documents = collection.documents.map do |record|
+      eager_loader.all = collection.map do |record|
         record.set_relation(arg, relations[record.send(arg_id)])
 
         record
       end
     end
 
-    collection
+    eager_loader
   end
 end
