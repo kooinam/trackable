aa50b286e575c3df1b33d1318d4faa3c6c792b6b
kooi nam ng <kooinamng@koois-MacBook-Air.local>
2019-01-09 01:00:34 +0800

    resultable

---
+++ app/models/resultable.rb
@@ -0,0 +1,20 @@
+class Resultable
+  attr_accessor :errors, :parameters
+
+  def initialize
+    self.errors = {}
+    self.parameters = {}
+  end
+
+  def add_error(key, value)
+    self.errors[key] = value
+  end
+
+  def success?
+    self.errors.empty?
+  end
+
+  def failed?
+    self.errors.empty? == false
+  end
+end
