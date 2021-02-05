529258028fe6d2a87c44ac79004e60cc33d86289
Gerald <gerald.chuah@kingmakergames.co>
2021-02-05 12:55:35 +0800

    update redis lock with timeout function

--- app/models/redis_lock.rb
+++ app/models/redis_lock.rb
@@ -1,14 +1,20 @@
 class RedisLock
   extend ActiveSupport::Concern
 
+  attr_accessor(
+    :key,
+    :expire,
+    :timeout,
+  )
+
   def self.get_instance
     broadcast_profile = RedisProfile.redis_instance
 
     redis = Redis.new(
-      url: broadcast_profile.url, 
-      password: broadcast_profile.password, 
+      url: broadcast_profile.url,
+      password: broadcast_profile.password,
       port: broadcast_profile.port,
-      timeout: broadcast_profile.timeout
+      timeout: broadcast_profile.timeout,
     )
 
     nsp = Redis::Namespace.new(broadcast_profile.namespace, redis: redis)
@@ -16,81 +22,144 @@ class RedisLock
     nsp
   end
 
-  def self.lock(key, lock: true, expire: 10)
-    res = false
-    start_at = DateTime.now
-    redis = self.get_instance
-
-    wait_duration = 0.01
-    expire_in = DateTime.now + expire.seconds
+  def self.lock(key, lock: true, expire: 10, timeout: nil)
+    redis_lock = RedisLock.new(key, expire, timeout)
+    response = false
 
     if lock
-      while redis.setnx(key, expire_in) == false
-        value = redis.get(key)
+      response = redis_lock.pending_lock()
+    else 
+      response = redis_lock.direct_lock()
+    end
 
-        if value
-          time = nil
+    redis_lock.close()
 
-          begin
-            time = DateTime.parse(value)
-          rescue Exception => e
-            time = DateTime.now
+    response
+  end
 
-            DevMessage.track("Redis LOCK Error #{value} #{key} #{e}", 'REDIS LOCK ERROR', important: true)
-          end
+  def self.unlock(key)
+    RedisLock.new(key).unlock
+  end
 
-          if DateTime.now >= time
-            DevMessage.track("Redis LOCK LOCKED #{key} #{start_at} #{time}", 'REDIS', important: true)
+  def initialize(key, expire: nil, timeout: nil)
+    self.key = key
+    self.expire = expire
+    self.timeout = timeout
+  end
 
-            Rails.logger.error "ALERT REDIS LOCK #{key}"
-            redis.del(key)
-          end
+  def pending_lock
+    response = false
+
+    redis = self.class.get_instance
+    key = self.key
+    expire = self.expire
+
+    start_at = DateTime.now
+    expire_in = DateTime.now + expire.seconds
+
+    wait_duration = 0.01
+    total_wait_duration = 0
+
+    # if the key is exist, keep waiting until it release
+    while redis.setnx(key, expire_in) == false
+      total_wait_duration += wait_duration
+
+      check_timeout(total_wait_duration, start_at)
+
+      value = redis.get(key)
+
+      if value
+        time = try_parse_datetime(value)
+
+        if DateTime.now >= time
+          DevMessage.track("Redis LOCK LOCKED #{key} #{start_at} #{time}", 'REDIS', important: true)
+
+          Rails.logger.error "ALERT REDIS LOCK #{key}"
+          redis.del(key)
         end
-
-        sleep wait_duration
       end
 
-      res = true
+      sleep wait_duration
+    end
+
+    response = true
+
+    response
+  end
+
+  def direct_lock
+    response = false
+
+    key = self.key
+    expire = self.expire
+
+    redis = self.class.get_instance
+
+    expire_in = DateTime.now + expire.seconds
+
+    if redis.setnx(key, expire_in)
+      response = true
     else
-      if redis.setnx(key, expire_in)
-        res = true
-      else
-        value = redis.get(key)
+      value = redis.get(key)
 
-        if value
-          time = nil
+      if value
+        time = try_parse_datetime(value)
 
-          begin
-            time = DateTime.parse(value)
-          rescue Exception => e
-            time = DateTime.now
+        if DateTime.now >= time
+          redis.del(key)
 
-            DevMessage.track("Redis LOCK Error #{value} #{key} #{e}", 'REDIS LOCK ERROR', important: true)
-          end
+          redis.setnx(key, expire_in)
 
-          if DateTime.now >= time
-            redis.del(key)
-
-            redis.setnx(key, expire_in)
-
-            res = true
-          else
-            res = false
-          end
+          response = true
+        else
+          response = false
         end
       end
     end
 
-    redis.redis.close
-
-    res
+    response
   end
 
-  def self.unlock(key)
-    redis = self.get_instance
+  def unlock
+    redis = self.class.get_instance
 
-    redis.del(key)
+    redis.del(self.key)
 
     redis.redis.close
+  end
+
+  def close 
+    redis = self.class.get_instance
+
+    redis.redis.close
+  end
+
+  private
+  def try_parse_datetime(value)
+    time = nil
+    key = self.key
+
+    begin
+      time = DateTime.parse(value)
+    rescue Exception => e
+      time = DateTime.now
+
+      DevMessage.track("Redis LOCK Error #{value} #{key} #{e}", 'REDIS LOCK ERROR', important: true)
+    end
+
+    time
+  end
+
+  def check_timeout(total_wait_duration, start_at)
+    timeout = self.timeout
+    key = self.key
+
+    if timeout.nil?
+      return
+    end
+
+    if total_wait_duration >= timeout
+      raise "Redis LOCK LOCKED #{key} #{start_at}"
+    end
   end
 end
